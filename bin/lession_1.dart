2912a58fc55a3567cef196452528b8e8d1840da6
QuangAnhTr9s <trinhquanganh210799@gmail.com>
2022-11-15 20:57:33 +0700

    Initial commit

---
+++ bin/lession_1.dart
@@ -0,0 +1,5 @@
+import 'package:lession_1/lession_1.dart' as lession_1;
+// 15/11/2022
+void main(List<String> arguments) {
+  print('Hello world: ${lession_1.calculate()}!');
+}
---
+++ test/lession_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:lession_1/lession_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
