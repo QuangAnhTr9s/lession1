0815972331ce8a5d84c6395beb66334a5202a22d
QuangAnhTr9s <trinhquanganh210799@gmail.com>
2022-11-21 17:04:34 +0700

    add file exercise.dart

--- bin/lession_1.dart
+++ bin/lession_1.dart
@@ -1,5 +1,47 @@
-import 'package:lession_1/lession_1.dart' as lession_1;
 // 15/11/2022
 void main(List<String> arguments) {
-  print('Hello world: ${lession_1.calculate()}!');
+  print('Hello world');
+  int a = 6;
+  String b = "120.34";
+  String c = "true";
+  double d = 120;
+
+  bool e = (c.toLowerCase() == "true") ? true : false;
+  print("e = $e");
+
+
+  print("x = ${a.toString()}");
+  print("y = ${double.parse(b)}");
+
+  if(c == "true"){
+    print("z = true");
+  }
+
+  print("k = ${d.toInt()}");
+  print("w = ${d.toString()}");
+
+  var ac = 12;
+  ac = 13;
+  print(ac);
+  dynamic acc = 12;
+  acc = "122";
+  print(acc);
+
 }
+void learnMap() {
+  final myInfo = {
+    'name': "Trinh Quang Anh",
+    'age': '23',
+    'level': 300,
+  };
+  print(myInfo['name']);
+}
+
+//BTVN:
+/*Bài 1:
+khai báo mảng Flutter3 gồm tên các bạn trong lớp. Sau đó thực hiện thêm, sửa tên 1 vài bạn.
+kbao 1 mảng ageOfMembers = tuổi của tất cả các bạn trong lớp => chuyển thành mảng String.*/
+/*
+Bài 2: về Map
+-Khai báo thông tin cá nhân (tên, tuổi, năm sinh, giới tính, sở thích). Sau đó thay đổi 1 vài thông tin
+*/
---
+++ lib/exercise.dart
@@ -0,0 +1,39 @@
+//BTVN:
+/*Bài 1:
+khai báo mảng Flutter3 gồm tên các bạn trong lớp. Sau đó thực hiện thêm, sửa tên 1 vài bạn.
+kbao 1 mảng ageOfMembers = tuổi của tất cả các bạn trong lớp => chuyển thành mảng String.*/
+/*
+Bài 2: về Map
+-Khai báo thông tin cá nhân (tên, tuổi, năm sinh, giới tính, sở thích). Sau đó thay đổi 1 vài thông tin
+*/
+
+void main(){
+  bai1();
+  bai2();
+}
+
+void bai1() {
+  List<String> flutter3 = ["Trinh Quang Anh", "Quang Anh", "Minh Hiếu", "Lam", "Vinh","Test"];
+  print("flutter 03: $flutter3");
+  flutter3.add("A"); //thêm học sinh A
+  flutter3[0] = "QA"; //sửa Trịnh Quang Anh thành QA
+  flutter3.remove("Test"); //xóa Test
+
+  print("flutter 03 sau khi thêm, sửa, xóa: $flutter3");
+
+  List<int> ageOfMembers = [23, 21, 27, 22, 27, 44];
+  print("ageOfMembers: $ageOfMembers");
+
+  //convert
+  List<String> ageOfMembersString = ageOfMembers.map((e) => e.toString()).toList();
+  print("ageOfMembersString: $ageOfMembersString");
+}
+void bai2() {
+  const myInfo = {
+    'name': 'Trinh Quang Anh',
+    'age' : '23',
+    'gender' : 'Male',
+    'interests': 'Music, Football, Technology',
+  };
+  print("My info: $myInfo");
+}
